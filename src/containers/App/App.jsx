2fd20a3450238c31adf7221040d5e550d2679134
Akhand Mishra <akkimysite@gmail.com>
2018-10-26 02:10:10 +0530

    Add Route for home-page

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Layout from '../../hoc/Layout/Layout';
+import Home from '../Home/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Layout>
-          
-        </Layout>
-      </div>
+      <BrowserRouter>  
+        <div className="App">
+          <Layout>
+            <Route path="/" exact component={Home} />
+          </Layout>
+        </div>
+      </BrowserRouter>
     );
   }
 }
