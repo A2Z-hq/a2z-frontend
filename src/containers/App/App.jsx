5bb19ed39c1e4c34a4dd18cd8eebb31d8a2021f2
Akhand Mishra <akkimysite@gmail.com>
2018-10-26 01:27:09 +0530

    Added Files

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Layout from '../../hoc/Layout/Layout';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Layout>
+          
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
