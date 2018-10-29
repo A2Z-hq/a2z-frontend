da1932cbbb3fbe8591f8fe5d0c350dad7957a4ab
Aditya Kolla <aditya.169108011@muj.manipal.edu>
2018-10-29 10:38:40 +0530

    Add CardLink

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './Home.css';
+import CardLink from '../../components/CardLink/CardLink'
 
 export default class Home extends React.Component {
     render() {
         return (
             <div id="home-page-container">
-                <h2>Add the home page here</h2>
+                <div className="grid">
+                    <div className="grid-item">
+                        <CardLink href="/">Python</CardLink>
+                    </div>
+                    <div className="grid-item">
+                        <CardLink bgColor="red" href="/">Javascript</CardLink> 
+                    </div>
+                    <div className="grid-item">
+                        <CardLink bgColor="green" href="/">Node.js</CardLink>
+                    </div>
+                    <div className="grid-item">
+                        <CardLink bgColor="#ac00e6" href="/">Machine Learning</CardLink>
+                    </div>
+                </div>
             </div>
         );
     }
